aa3bde78e25a7ccd10eab683bc29d84722de6d60
Abhinav Mathur <142311639+Alexander1947@users.noreply.github.com>
2023-08-20 17:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 //your code here
-#btn{
-	background-color:black;
-	color:white;
-	border-radius:5px;
-	border:none;
-}
-#btn:hover{
-	background-color:yellow;
-	color:black;
-	border-radius:30px;
-}
+
